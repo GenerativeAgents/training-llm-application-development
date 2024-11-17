473cb321ef4e3d1a76dce44a34f68cf0b3aed2b3
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2024-11-17 16:38:12 +0900

    Add hybrid rag chain

--- core/rag/factory.py
+++ core/rag/factory.py
@@ -3,6 +3,7 @@ from typing import Any
 
 from langchain_core.runnables import Runnable
 
+from core.rag.chains.hybrid import create_hybrid_rag_chain
 from core.rag.chains.hyde import create_hyde_rag_chain
 from core.rag.chains.multi_query import create_multi_query_rag_chain
 from core.rag.chains.naive import create_naive_rag_chain
@@ -18,6 +19,7 @@ class RAGChainType(str, Enum):
     RAG_FUSION = "fag_fusion"
     RERANK = "rerank"
     ROUTE = "route"
+    HYBRID = "hybrid"
 
 
 def create_rag_chain(rag_chain_type: RAGChainType) -> Runnable[str, dict[str, Any]]:
@@ -33,6 +35,8 @@ def create_rag_chain(rag_chain_type: RAGChainType) -> Runnable[str, dict[str, An
         chain = create_rerank_rag_chain()
     elif rag_chain_type == RAGChainType.ROUTE:
         chain = create_route_rag_chain()
+    elif rag_chain_type == RAGChainType.HYBRID:
+        chain = create_hybrid_rag_chain()
     else:
         raise ValueError(f"Unknown RAG chain type: {rag_chain_type}")
 
