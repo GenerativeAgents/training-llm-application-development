452ce19863e619d3202a0b17f8f783964f11fbe3
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2024-11-17 15:06:51 +0900

    Add multi query retriever

--- core/rag/factory.py
+++ core/rag/factory.py
@@ -4,12 +4,14 @@ from typing import Any
 from langchain_core.runnables import Runnable
 
 from core.rag.hyde import create_hyde_rag_chain
+from core.rag.multi_query import create_multi_query_rag_chain
 from core.rag.naive import create_naive_rag_chain
 
 
 class RAGChainType(str, Enum):
     NAIVE = "naive"
     HYDE = "hyde"
+    MULTI_QUERY = "multi_query"
 
 
 def create_rag_chain(rag_chain_type: RAGChainType) -> Runnable[str, dict[str, Any]]:
@@ -17,5 +19,7 @@ def create_rag_chain(rag_chain_type: RAGChainType) -> Runnable[str, dict[str, An
         return create_naive_rag_chain()
     elif rag_chain_type == RAGChainType.HYDE:
         return create_hyde_rag_chain()
+    elif rag_chain_type == RAGChainType.MULTI_QUERY:
+        return create_multi_query_rag_chain()
     else:
         raise ValueError(f"Unknown RAG chain type: {rag_chain_type}")
